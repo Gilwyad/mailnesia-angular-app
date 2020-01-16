25bab8927b0c4ce2dcb34f59168eed5d843f26fd
Peter Baranyi <peter@gilwyad.com>
2020-01-16 23:00:35 +0100

    Move routes to app routing module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,21 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { MailboxSettingsComponent } from './mailbox-settings/mailbox-settings.component';
+import { MailboxListViewComponent } from './mailbox-list-view/mailbox-list-view.component';
+import { MailboxDetailViewComponent } from './mailbox-detail-view/mailbox-detail-view.component';
+import { MainPageComponent } from './main-page/main-page.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: MainPageComponent },
+  { path: 'settings/:mailbox', component: MailboxSettingsComponent },
+  { path: 'mailbox/:mailbox', component: MailboxListViewComponent },
+  { path: 'mailbox/:mailbox/:emailId', component: MailboxDetailViewComponent },
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [
+    RouterModule.forRoot(routes, { useHash: true })
+  ],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,16 +9,8 @@ import { MailboxSettingsComponent } from './mailbox-settings/mailbox-settings.co
 import { MailboxListViewComponent } from './mailbox-list-view/mailbox-list-view.component';
 import { MailboxDetailViewComponent } from './mailbox-detail-view/mailbox-detail-view.component';
 import { MainPageComponent } from './main-page/main-page.component';
-import { RouterModule, Routes } from '@angular/router';
 import { MailboxFormComponent } from './forms/mailbox-form/mailbox-form.component';
 import { MailboxNavigationFormComponent } from './forms/mailbox-navigation-form/mailbox-navigation-form.component';
-
-const routes: Routes = [
-  { path: '', component: MainPageComponent },
-  { path: 'settings/:mailbox', component: MailboxSettingsComponent },
-  { path: 'mailbox/:mailbox', component: MailboxListViewComponent },
-  { path: 'mailbox/:mailbox/:emailId', component: MailboxDetailViewComponent },
-];
 
 @NgModule({
   declarations: [
@@ -35,7 +27,6 @@ const routes: Routes = [
     AppRoutingModule,
     ReactiveFormsModule,
     BrowserAnimationsModule,
-    RouterModule.forRoot(routes, { useHash: true })
   ],
   providers: [
   ],
