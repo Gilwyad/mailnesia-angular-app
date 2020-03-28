7f2aa28943fd4b6b7bc87f32e1adb6b4bd7af901
Peter Baranyi <peter@gilwyad.com>
2020-03-28 14:39:40 +0100

    Add placeholder for mailbox settings page

--- src/app/mailbox-settings/mailbox-settings.component.spec.ts
+++ src/app/mailbox-settings/mailbox-settings.component.spec.ts
@@ -1,6 +1,6 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { MailboxSettingsComponent } from './mailbox-settings.component';
+import { RouterTestingModule } from '@angular/router/testing';
 
 describe('MailboxSettingsComponent', () => {
   let component: MailboxSettingsComponent;
@@ -8,7 +8,10 @@ describe('MailboxSettingsComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ MailboxSettingsComponent ]
+      declarations: [ MailboxSettingsComponent ],
+      imports: [
+        RouterTestingModule,
+      ]
     })
     .compileComponents();
   }));
--- src/app/mailbox-settings/mailbox-settings.component.ts
+++ src/app/mailbox-settings/mailbox-settings.component.ts
@@ -1,4 +1,6 @@
+import { ActivatedRoute } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
+import {Location} from '@angular/common';
 
 @Component({
   selector: 'app-mailbox-settings',
@@ -6,10 +8,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./mailbox-settings.component.less']
 })
 export class MailboxSettingsComponent implements OnInit {
+  mailbox: string;
 
-  constructor() { }
+  constructor(
+    private location: Location,
+    private route: ActivatedRoute,
+  ) { }
 
   ngOnInit() {
+    this.route.params.subscribe(
+      params => {
+        this.mailbox = params.mailbox;
+      }
+    );
   }
 
+  goBack() {
+    this.location.back();
+  }
 }
