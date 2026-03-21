3b750ec007fc7fa2bb8660ffdefc17fb69957e58
Peter Baranyi <peter.baranyi@protonmail.com>
2026-03-21 01:49:18 +0100

    fix NG0100 errors
    https://stackoverflow.com/questions/79837996/errors-in-unit-tests-after-updating-to-angular-v21

--- src/test.ts
+++ src/test.ts
@@ -1,16 +1,20 @@
 // This file is required by karma.conf.js and loads recursively all the .spec and framework files
 
 import 'zone.js/testing';
+import { NgModule, provideZoneChangeDetection } from '@angular/core';
 import { getTestBed } from '@angular/core/testing';
 import {
-  BrowserDynamicTestingModule,
-  platformBrowserDynamicTesting
-} from '@angular/platform-browser-dynamic/testing';
+  BrowserTestingModule,
+  platformBrowserTesting,
+} from '@angular/platform-browser/testing';
 
-// First, initialize the Angular testing environment.
+
+@NgModule({
+  providers: [provideZoneChangeDetection()],
+})
+class AppTestingModule {}
+
 getTestBed().initTestEnvironment(
-  BrowserDynamicTestingModule,
-  platformBrowserDynamicTesting(), {
-    teardown: { destroyAfterEach: false }
-}
+  [BrowserTestingModule, AppTestingModule],
+  platformBrowserTesting(),
 );
